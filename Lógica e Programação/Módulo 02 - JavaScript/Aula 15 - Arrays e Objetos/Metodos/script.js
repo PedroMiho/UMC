bec263c7cae1f99d48795e1074aea7d7a0af3dbb
Pedro Henrique Miho de Souza <pedromihosoza@gmail.com>
2025-03-19 17:38:30 -0300

    .

---
+++ Lógica e Programação/Módulo 02 - JavaScript/Aula 15 - Arrays e Objetos/Metodos/script.js
@@ -0,0 +1,14 @@
+let times = ["Santos", "São Paulo", "Palmeiras"]
+
+//Acessar um item da lista
+console.log(times[0]);
+
+//Verificando o tamanho da lista
+console.log(times.length); //3
+
+//Adicionando itens no Array
+times.push("Ponte Preta") //["Santos", "São Paulo", "Palmeiras","Ponte Preta"]
+
+//Adicionando itens no inicio do array
+times.unshift("Guarani")
+console.log(times);
