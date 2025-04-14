e8dbb904703f1144762f9848bb5d098fd51f9ae0
Pedro Henrique Miho de Souza <pedromihosoza@gmail.com>
2025-04-14 08:07:19 -0300

    .

--- Projetos de Software/Aula 06 - Introdução à POO/Exemplo/src/Estruturada/ContaMes.java
+++ Projetos de Software/Aula 06 - Introdução à POO/Exemplo/src/Estruturada/ContaMes.java
@@ -5,35 +5,39 @@ import java.util.Scanner;
 public class ContaMes {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		double alimJan, energiaJan, aguaJan, alimFev, energiaFev, aguaFev;
+//		
+//		System.out.println("Digite as contas de Janeiro");
+//		alimJan = sc.nextDouble();
+//		energiaJan = sc.nextDouble();
+//		aguaJan = sc.nextDouble();
+//		System.out.println("Digite as contas de Fevereiro");
+//		alimFev = sc.nextDouble();
+//		energiaFev = sc.nextDouble();
+//		aguaFev = sc.nextDouble();
+//		
+//		double contaJan = aguaJan + alimJan + energiaJan;
+//		double contaFev = aguaFev + alimFev + energiaFev;
+//		
+//		if (contaJan > contaFev) {
+//			System.out.println("A conta de Janeiro é maior do que a de Fevereiro");
+//		}
+//		
+//		else if (contaFev > contaJan) {
+//			System.out.println("A conta de Fevereiro é maior do que a de Janeiro");
+//		}
+//		else {
+//			System.out.println("Ambas as contas possuem o mesmo valor");
+//		}
+//		
+//		sc.close();
 		
-		double alimJan, energiaJan, aguaJan, alimFev, energiaFev, aguaFev;
 		
-		System.out.println("Digite as contas de Janeiro");
-		alimJan = sc.nextDouble();
-		energiaJan = sc.nextDouble();
-		aguaJan = sc.nextDouble();
-		System.out.println("Digite as contas de Fevereiro");
-		alimFev = sc.nextDouble();
-		energiaFev = sc.nextDouble();
-		aguaFev = sc.nextDouble();
-		
-		double contaJan = aguaJan + alimJan + energiaJan;
-		double contaFev = aguaFev + alimFev + energiaFev;
-		
-		if (contaJan > contaFev) {
-			System.out.println("A conta de Janeiro é maior do que a de Fevereiro");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
 		}
-		
-		else if (contaFev > contaJan) {
-			System.out.println("A conta de Fevereiro é maior do que a de Janeiro");
-		}
-		else {
-			System.out.println("Ambas as contas possuem o mesmo valor");
-		}
-		
-		sc.close();
-
 	}
 
 }
