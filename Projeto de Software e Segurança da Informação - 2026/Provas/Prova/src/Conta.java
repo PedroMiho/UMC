e0ad40b3d67629236f0ea5a472b75c3087b2fc77
Pedro Henrique Miho de Souza <pedromihosoza@gmail.com>
2026-04-28 07:30:39 -0300

    .

---
+++ Projeto de Software e Segurança da Informação - 2026/Provas/Prova/src/Conta.java
@@ -0,0 +1,7 @@
+public class Conta {
+    private double saldo;
+
+    public Conta(double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ Projeto de Software e Segurança da Informação - 2026/Provas/Prova/src/Dispositivo.java
@@ -0,0 +1,14 @@
+public class Dispositivo {
+    public String tipo;
+    public double valor;
+
+    public Dispositivo( /*Lacuna 1*/ ){
+        /*
+        Lacuna 2
+        */
+    }
+
+    public void exibirFicha() {
+        System.out.println("Equipamento: " + tipo + " | Valor: R$ " + valor);
+    }
+}
