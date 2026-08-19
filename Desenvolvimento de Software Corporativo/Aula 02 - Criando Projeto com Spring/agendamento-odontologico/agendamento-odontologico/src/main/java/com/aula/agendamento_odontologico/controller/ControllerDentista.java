2b46e8ca3409b2ff363df26c06bc88ec167eeb2a
PedroMiho <pedromihosoza@gmail.com>
2026-08-19 18:38:37 -0300

    .

--- Desenvolvimento de Software Corporativo/Aula 02 - Criando Projeto com Spring/agendamento-odontologico/agendamento-odontologico/src/main/java/com/aula/agendamento_odontologico/controller/ControllerDentista.java
+++ Desenvolvimento de Software Corporativo/Aula 02 - Criando Projeto com Spring/agendamento-odontologico/agendamento-odontologico/src/main/java/com/aula/agendamento_odontologico/controller/ControllerDentista.java
@@ -1,15 +1,15 @@
 package com.aula.agendamento_odontologico.controller;
 
 import com.aula.agendamento_odontologico.dto.DadosCadastroDentista;
+import com.aula.agendamento_odontologico.dto.DadosListagemDentista;
 import com.aula.agendamento_odontologico.model.Dentista;
 import com.aula.agendamento_odontologico.repository.DentistaRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/dentista")
 @RestController
@@ -22,5 +22,10 @@ public class ControllerDentista {
     @Transactional
     public void cadastrarDentista(@RequestBody @Valid  DadosCadastroDentista dadosDentista) {
         repository.save(new Dentista(dadosDentista));
+    }
+
+    @GetMapping
+    public List<DadosListagemDentista> listaDentista(){
+        return repository.findAll().stream().map(DadosListagemDentista::new).toList();
     }
 }
---
+++ Desenvolvimento de Software Corporativo/Aula 02 - Criando Projeto com Spring/agendamento-odontologico/agendamento-odontologico/src/main/java/com/aula/agendamento_odontologico/dto/DadosListagemDentista.java
@@ -0,0 +1,18 @@
+package com.aula.agendamento_odontologico.dto;
+
+import com.aula.agendamento_odontologico.enums.Especialidade;
+import com.aula.agendamento_odontologico.model.Dentista;
+
+public record DadosListagemDentista(
+        String nome,
+        String email,
+        String cro,
+        Especialidade especialidade
+) {
+
+
+
+    public DadosListagemDentista(Dentista dentista) {
+        this(dentista.getNome(), dentista.getEmail(), dentista.getCro(), dentista.getEspecialidade());
+    }
+}
