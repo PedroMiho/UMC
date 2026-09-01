c9bc14cb7e0ec9178cd45a6b45d13a20d85dc797
Pedro Henrique Miho de Souza <pedromihosoza@gmail.com>
2026-09-01 17:53:06 -0300

    .

--- SI/Programação para Internet/Módulo 2 - JavaScript/Aula03 - Implementado o CRUD/codigo/js/api.js
+++ SI/Programação para Internet/Módulo 2 - JavaScript/Aula03 - Implementado o CRUD/codigo/js/api.js
@@ -1,17 +1,10 @@
 const api = {
-
     async buscarProdutos() {
-
         try {
-
             const response = await fetch("http://localhost:3000/produtos")
-
             return await response.json()
-
         } catch (error) {
-
             alert("Erro ao buscar produtos")
-
             throw error
         }
     }
