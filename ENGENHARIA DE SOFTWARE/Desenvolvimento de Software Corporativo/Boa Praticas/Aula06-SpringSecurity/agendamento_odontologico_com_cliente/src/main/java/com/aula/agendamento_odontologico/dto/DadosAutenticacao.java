33bacc91ba3f012a892fb2a840782019e8afb332
Pedro Henrique Miho de Souza <106999878+PedroMiho@users.noreply.github.com>
2026-09-09 14:27:41 -0300

    Delete ENGENHARIA DE SOFTWARE/Desenvolvimento de Software Corporativo/Boa Praticas/Aula06-SpringSecurity/agendamento_odontologico_com_cliente directory

--- ENGENHARIA DE SOFTWARE/Desenvolvimento de Software Corporativo/Boa Praticas/Aula06-SpringSecurity/agendamento_odontologico_com_cliente/src/main/java/com/aula/agendamento_odontologico/dto/DadosAutenticacao.java
+++
@@ -1,7 +0,0 @@
-package com.aula.agendamento_odontologico.dto;
-
-public record DadosAutenticacao(
-        String login,
-        String senha
-) {
-}
--- ENGENHARIA DE SOFTWARE/Desenvolvimento de Software Corporativo/Boa Praticas/Aula06-SpringSecurity/agendamento_odontologico_com_cliente/src/main/resources/db/migration/V4__create-table-usuarios.sql
+++
@@ -1,8 +0,0 @@
-create table usuarios(
-
-    id bigint not null auto_increment primary key,
-    login varchar(100) not null,
-    senha varchar(100) not null
-
-);
-
