525fd89876029a4ff7eb16d78c6404a375510e69
Pedro Henrique Miho de Souza <106999878+PedroMiho@users.noreply.github.com>
2026-09-11 08:35:25 -0300

    chore: remove artefatos gerados e adiciona gitignore global

--- ENGENHARIA DE SOFTWARE/Desenvolvimento de Software Corporativo/Boa Praticas/Aula05-TratamentoErros/agendamento_odontologico_com_cliente/target/classes/db/migration/V2__alter-table-dentista-add-column-ativo.sql
+++
@@ -1,2 +0,0 @@
-alter table dentista add ativo tinyint;
-update dentista set ativo = 1;
--- ENGENHARIA DE SOFTWARE/Desenvolvimento de Software Corporativo/Boa Praticas/Aula05-TratamentoErros/agendamento_odontologico_com_cliente/target/classes/db/migration/v4__create-table-usuarios.sql
+++
@@ -1,6 +0,0 @@
-create table cliente(
-
-        id bigint not null auto_increment primary key,
-        login varchar(100) not null  unique,
-        senha varchar(100) not null
-);
